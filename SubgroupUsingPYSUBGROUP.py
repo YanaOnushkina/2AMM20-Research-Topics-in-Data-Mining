d6aff8413e24ed5a686d460676e0aa1eae5570f9
YanaOnushkina <y.g.onushkina@student.tue.nl>
2021-10-18 17:58:34 +0200

    A working implementation but with Binary target

---
+++ SubgroupUsingPYSUBGROUP.py
@@ -0,0 +1,23 @@
+import pysubgroup as ps
+import pandas as pd
+
+# Load the example dataset
+data = pd.read_csv("data/BrexitAttitudes_Data_wave1.csv")
+
+#ps.NumericTarget() -> read the documentation
+target = ps.BinaryTarget('BrexitID', 'a Remainer')
+
+searchspace = ps.create_selectors(data, ignore=['BrexitID'])
+task = ps.SubgroupDiscoveryTask (
+    data,
+    target,
+    searchspace,
+    result_set_size=5,
+    depth=4,
+    qf=ps.WRAccQF())
+result = ps.BeamSearch().execute(task)
+
+rules = result.to_dataframe()
+
+rules.to_csv("./rules/Rules wave1")
+
