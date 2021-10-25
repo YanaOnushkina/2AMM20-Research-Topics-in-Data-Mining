e2f4e21a75a734a531109fe1f5fc2a7b4982c1ff
YanaOnushkina <y.g.onushkina@student.tue.nl>
2021-10-25 22:39:27 +0200

    Cleaned data and rule files

--- SubgroupUsingPYSUBGROUP.py
+++ SubgroupUsingPYSUBGROUP.py
@@ -2,7 +2,7 @@ import pysubgroup as ps #from: https://github.com/flemmerich/pysubgroup/tree/mas
 import pandas as pd
 
 # Load the example dataset
-data = pd.read_csv("../data/BrexitAttitudes_Data_wave1.csv")
+data = pd.read_csv("data/BrexitAttitudes_Data_wave10_clean.csv")
 
 #ps.NumericTarget() -> read the documentation
 target = ps.BinaryTarget('BrexitID', 'a Remainer')
@@ -12,12 +12,12 @@ task = ps.SubgroupDiscoveryTask (
     data,
     target,
     searchspace,
-    result_set_size=5,
-    depth=4,
+    result_set_size=4,
+    depth=10,
     qf=ps.WRAccQF())
 result = ps.BeamSearch().execute(task)
 
 rules = result.to_dataframe()
 
-rules.to_csv("./rules/Rules wave1")
+rules.to_csv("./rules/Rules wave10")
 
